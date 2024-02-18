732bf47073816d99205e99aff44e6217daa51eea
Osmar Alves Cardoso Junior <osmar.alves.cardoso@gmail.com>
2024-02-18 19:00:33 -0300

    create archive response and refact remove comments

--- cmd/api/config/database.go
+++ cmd/api/config/database.go
@@ -24,7 +24,6 @@ func database() {
 	}
 	defer db.Close()
 
-	// Verificar se a conexão com o banco de dados está funcionando
 	err = db.Ping()
 	if err != nil {
 		log.Fatalf("Error pinging the database: %v\n", err)
@@ -32,20 +31,15 @@ func database() {
 
 	fmt.Println("Connected to the database!")
 
-	// Agora você pode usar 'db' para executar consultas SQL
-	// Por exemplo:
 	rows, err := db.Query("SELECT * FROM your_table")
 	if err != nil {
 		log.Fatalf("Error querying the database: %v\n", err)
 	}
 	defer rows.Close()
 
-	// Processar os resultados da consulta, se necessário
 	for rows.Next() {
-		// Processar cada linha retornada
 	}
 
-	// Verificar se houve algum erro durante o processamento das linhas
 	if err := rows.Err(); err != nil {
 		log.Fatalf("Error processing rows: %v\n", err)
 	}
