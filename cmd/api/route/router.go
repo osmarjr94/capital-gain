742cdc68a77872e6108535271656175d61caecf2
Osmar Alves Cardoso Junior <osmar.alves.cardoso@gmail.com>
2024-02-18 20:42:49 -0300

    create route

--- cmd/api/route/router.go
+++ cmd/api/route/router.go
@@ -1,1 +1,25 @@
 package route
+
+import (
+	"echo"
+	"net/http"
+
+	"github.com/osmarjr94/capital-gain/cmd/api/internal/models"
+)
+
+func RegisterRoutes(e *echo.Echo, controller *OperationController) {
+	e.POST("/operation", func(c echo.Context) error {
+		var operation []models.Operation
+
+		if err := c.Bind(&operation); err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]string{"error": "Erro ao decodificar o corpo da solicitação"})
+		}
+
+		tax, err := controller.HandleOperation(operation)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Erro ao processar a operação"})
+		}
+
+		return c.JSON(http.StatusOK, map[string]int{"tax": tax})
+	})
+}
